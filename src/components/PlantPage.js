7c5b91e43c7b5b84c70074b73173805d4a149815
Caleb <calebkiune@gmail.com>
2024-11-08 22:25:37 +0300

    Adds first commit

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((response) => response.json())
+    .then((data) => { 
+      console.log("Fetched plants data:", data);
+       setPlants(data); })
+    .catch((error) => console.error ('Error fetching plants:', error))
+  }, [])
+
+
+  function handleAddPlant(newPlant){
+    setPlants([...plants,newPlant])
+  }
+
+  const filteredPlants = plants.filter(plant =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+
+
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search setSearchTerm={setSearchTerm}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
